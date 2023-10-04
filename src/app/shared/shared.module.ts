4b3f481fbe8d6c204d7bab8911f8dd4cfdb84605
krezoiv <erick.adan@gmail.com>
2023-10-03 21:15:27 -0600

    031023

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainSidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
